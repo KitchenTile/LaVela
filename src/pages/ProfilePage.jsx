f1a9f15b50613e49f87b7aa3ff6a0405eb502a1a
KitchenTile <azuldebenedetti@gmail.com>
2025-03-31 21:29:37 +0100

    Create ProfilePage.jsx

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/header/Header";
+import Hero from "../components/main-page/Hero/Hero";
+import Gallery from "../components/main-page/image-gallery/Gallery";
+
+const MainPage = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Gallery />
+    </>
+  );
+};
+
+export default MainPage;
